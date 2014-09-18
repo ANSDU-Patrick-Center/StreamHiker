da114e38408b0fcd2fec33204e2b915ba5174a8b
Alexander M. Waldman <amw47@drexel.edu>
2014-09-18 14:15:29 -0400

    Create MakeTempFileV12.F90

---
+++ MakeTempFileV12.F90
@@ -0,0 +1,33 @@
+PROGRAM MakeTemp
+IMPLICIT None
+INTEGER::Loop
+INTEGER(8)::Row,Col, RecId, NRows, NCols, StartPos
+
+! ***************************************************** Full version *****************************************************
+OPEN(1, file = "output_specs2.txt", FORM = "FORMATTED", STATUS = "OLD")
+
+READ(1,*)
+READ(1,*) 
+READ(1,*)
+READ(1,3) NCols
+!WRITE(*,*) NCols
+READ(1,3) NRows
+!WRITE(*,*) NRows
+
+3 FORMAT(i8)
+
+! ********************************************************************************************************************
+OPEN(2, file = "RchSrch.flt"  , FORM = "binary" , Status = "UNKNOWN" , access = "direct",RECL = 4)
+!**************************************************************************************************************************     
+
+RecID    = 0
+StartPos = 0
+
+DO Row = 1,NRows !This the # of rows in the DEM and flow direction map
+   DO Col = 1,Ncols !This is the # of columns in the DEM and flow direction map 
+      RecID = (((Row-1)*NCols)+Col)
+      WRITE(2,rec = RecID) StartPos
+   END DO
+END DO
+
+END PROGRAM MakeTemp
